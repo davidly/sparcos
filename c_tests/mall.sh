e1c6e98213c2f88b22a1be1a38957f63359d2c85
davidly <davidly_lee@hotmail.com>
2025-09-03 10:26:53 -0700

    build fopentst too

--- c_tests/mall.sh
+++ c_tests/mall.sh
@@ -7,13 +7,11 @@ else
     optflags="$1"
 fi
 
-# not: fopentst
-
 for arg in hidave tprintf tm tmuldiv ttt sieve e tstr targs tbits t tao \
            tcmp ttypes tarray trw trw2 terrno mm_old ttime fileops tpi \
            t_setjmp td tf tap tphi mm ts glob nantst pis tfo sleeptm \
            nqueens nq1d tdir  lenum tex trename triangle fact tld ff \
-           an ba;
+           an ba fopentst;
 do
     echo $arg
     m.sh $arg $optflags
