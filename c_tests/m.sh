ac65be3835c2d7debb489a292e9049813ba34526
davidly <davidly_lee@hotmail.com>
2025-09-03 08:13:02 -0700

    don't copy test binaries by default

--- c_tests/m.sh
+++ c_tests/m.sh
@@ -18,6 +18,6 @@ gccpath="../gcc-14.3.0/bin"
 $gccpath/sparc-linux-gcc -x c++ -O$optlevel $1.c -o $1.elf -l:libstdc++.a -static
 $gccpath/sparc-buildroot-linux-uclibc-objdump -d $1.elf >$1.txt
 
-cp $1.c /mnt/c/users/david/onedrive/sparcos/c_tests
-cp $1.elf /mnt/c/users/david/onedrive/sparcos/c_tests
-cp $1.txt /mnt/c/users/david/onedrive/sparcos/c_tests
+#cp $1.c /mnt/c/users/david/onedrive/sparcos/c_tests
+#cp $1.elf /mnt/c/users/david/onedrive/sparcos/c_tests
+#cp $1.txt /mnt/c/users/david/onedrive/sparcos/c_tests
