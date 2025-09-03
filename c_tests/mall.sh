af516cb3dd35670ce60301b2e0af521c7e255ec3
davidly <davidly_lee@hotmail.com>
2025-09-03 08:14:38 -0700

    add fopentst

--- c_tests/mall.sh
+++ c_tests/mall.sh
@@ -7,12 +7,10 @@ else
     optflags="$1"
 fi
 
-# not: fopentst
-
 for arg in hidave tprintf tm tmuldiv ttt sieve e tstr targs tbits t tao \
            tcmp ttypes tarray trw trw2 terrno mm_old ttime fileops tpi \
            t_setjmp td tf tap tphi mm ts glob nantst pis tfo sleeptm \
-           nqueens nq1d tdir  lenum tex trename triangle fact tld ff
+           nqueens nq1d tdir  lenum tex trename triangle fact tld ff fopentst
 do
     echo $arg
     m.sh $arg $optflags
