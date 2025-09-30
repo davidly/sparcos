c1b5857324033ad527fcdbcd25fd55f91e542eab
davidly <davidly_lee@hotmail.com>
2025-09-30 08:25:50 -0700

    add mmap test that was missed

--- c_tests/runall.bat
+++ c_tests/runall.bat
@@ -35,7 +35,7 @@ echo %date% %time% >%outputfile%
 set _elflist=hidave tprintf tm tmuldiv ttt sieve e tstr targs tbits t tao ^
              tcmp ttypes tarray trw trw2 terrno mm_old ttime fileops tpi ^
              t_setjmp td tf tap tphi mm ts glob nantst pis tfo sleeptm ^
-             nqueens nq1d tdir fopentst lenum trename triangle fact tld ^
+             nqueens nq1d tdir fopentst lenum trename triangle fact tld tmmap ^
              esp sievesp tttsp tttusp
 
 ( for %%a in (%_elflist%) do (
