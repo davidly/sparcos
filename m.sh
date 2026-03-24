84aabcb6db3766670945b4ea7843841056d5e2cf
davidly <davidly_lee@hotmail.com>
2026-03-24 12:25:49 -0700

    build fix for non-amd64 linux builds

--- m.sh
+++ m.sh
@@ -1,1 +1,8 @@
-g++ -DSPARCOS -O3 -fno-builtin -I . sparcos.cxx sparc.cxx -o sparcos -static -lquadmath
+ARCH=$(uname -m)
+LDFLAGS="-lm"
+
+if [ "$ARCH" = "x86_64" ]; then
+            LDFLAGS="$LDFLAGS -lquadmath"
+fi
+
+g++ -DSPARCOS -O3 -fno-builtin -I . sparcos.cxx sparc.cxx -o sparcos -static $LDFLAGS
