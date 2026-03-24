a9f32c600835300339457429206c0302b46df3a2
davidly <davidly_lee@hotmail.com>
2026-03-24 13:53:28 -0700

    add build script for tests that don't use the C runtime

---
+++ c_tests/maa.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+#set -x
+
+if [ "$1" == "" ]; then
+    echo "Usage: m.sh <sourcefile> [optlevel]"
+    echo "  optlevel: 0, 1, 2 (default), 3, fast"
+    exit 1
+fi
+
+if ! test -f "$1.s"; then
+    echo "File $1.s not found!"
+    exit 1
+fi
+
+if [ "$2" == "" ]; then
+    optlevel="2"
+else
+    optlevel="$2"
+fi
+
+gccpath="../gcc-14.3.0/bin"
+
+# use -mcpu=v7 to generate mulscc instructions for integer multiplication
+$gccpath/sparc-linux-gcc -nostdlib -mcpu=v8 $1.s -o $1.elf -static
+
+# generate disassembly
+$gccpath/sparc-buildroot-linux-uclibc-objdump -d $1.elf >$1.txt
+
+cp $1.s /mnt/c/users/david/onedrive/sparcos/c_tests
+cp $1.elf /mnt/c/users/david/onedrive/sparcos/c_tests
+cp $1.txt /mnt/c/users/david/onedrive/sparcos/c_tests
